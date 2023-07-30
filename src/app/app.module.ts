da4581c58686028010708b12105c1f639afe0bc3
Gabriela Lima <gabrielalima558@gmail.com>
2023-07-30 10:19:01 -0300

    cria componente de cadastramento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EyHeaderComponent } from './components/ey-header/ey-header.component';
+import { PersonalInformationComponent } from './components/registration/personal-information/personal-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EyHeaderComponent
+    EyHeaderComponent,
+    PersonalInformationComponent
   ],
   imports: [
     BrowserModule,
