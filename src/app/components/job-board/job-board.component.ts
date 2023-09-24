56770694193d1f61a6de02a23753e75cefcf0abb
Gabriela Lima <gabrielalima558@gmail.com>
2023-09-24 12:01:26 -0300

    filtro por grupo

--- src/app/components/job-board/job-board.component.ts
+++ src/app/components/job-board/job-board.component.ts
@@ -11,15 +11,15 @@ import { JobsService } from '../service/jobs.service';
 export class JobBoardComponent implements OnInit {
 
   listJobs: Job[] = [];
-  filteredJobs: Job[] = []; // Lista de vagas filtradas
+  filteredJobs: Job[] = [];
   selectedItemDropdown: string = '';
+  selectedMinorityGroup: string[] = [];
 
   constructor(private service: JobsService) { }
 
   ngOnInit(): void {
     this.service.listar().subscribe((listJobs) => {
       this.listJobs = listJobs;
-      // Inicialmente, você pode mostrar todas as vagas
       this.filteredJobs = [...listJobs];
     });
   }
@@ -27,13 +27,42 @@ export class JobBoardComponent implements OnInit {
   onSelectDropdownItem(item: string) {
     this.selectedItemDropdown = item;
 
-    // Aqui você filtra a lista de vagas com base na opção selecionada
     if (item === 'Todas') {
-      // Se 'Todas' for selecionada, mostre todas as vagas
       this.filteredJobs = [...this.listJobs];
     } else {
-      // Caso contrário, filtre as vagas com base na opção selecionada
       this.filteredJobs = this.listJobs.filter((job) => job.name_position === item);
     }
   }
+
+  // ...
+
+// Esta função verifica se pelo menos um grupo minoritário selecionado está presente na lista de grupos da vaga
+isSelectedGroupInJob(selectedGroups: string[], job: Job): boolean {
+  return selectedGroups.some((group) => job.minority_group_type.includes(group));
+}
+
+onSelectMinorityGroup(group: string) {
+  // Atualize a lista de grupos selecionados
+  if (group === 'Todas') {
+    this.selectedMinorityGroup = [];
+  } else if (this.selectedMinorityGroup.includes(group)) {
+    // Se o grupo já estiver selecionado, remova-o
+    this.selectedMinorityGroup = [];
+    this.selectedMinorityGroup = this.selectedMinorityGroup.filter((g) => g !== group);
+  } else {
+    // Caso contrário, adicione-o à lista de grupos selecionados
+    this.selectedMinorityGroup.push(group);
+  }
+
+  // Filtrar a lista de vagas com base nos grupos selecionados
+  if (this.selectedMinorityGroup.length === 0) {
+    // Se nenhum grupo estiver selecionado, mostre todas as vagas
+    this.filteredJobs = [...this.listJobs];
+  } else {
+    this.filteredJobs = this.listJobs.filter((job) =>
+      this.isSelectedGroupInJob(this.selectedMinorityGroup, job)
+    );
+  }
+}
+
 }
